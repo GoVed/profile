a4f88b34582c1f2d10f9f6f7278da3469135cc9d
Ved Suthar <vedhsuthar@gmail.com>
2024-02-17 20:12:25 -0500

    states while animating sidebar

--- site/scripts/anim.js
+++ site/scripts/anim.js
@@ -13,7 +13,7 @@ async function startMakeAnim(result,element){
     const res=10
     const cps=(Array.from(result).length-1)/res    
 
-
+    start_state = current_state;
     for(var i=1;i<=res;i++){  
         //Intital starting original substring of result   
         var out=Array.from(result).slice(0, cps*i).join('')   
@@ -31,6 +31,9 @@ async function startMakeAnim(result,element){
 
         element.innerHTML=out
         await delay(totalTime/res)
+        if (start_state != current_state) {
+            return;
+        }
     }
   
     //After the animation
@@ -51,7 +54,7 @@ async function startDelAnim(element){
     const totalTime=500
     const res=10
     const cps=(Array.from(result).length-1)/res
-
+    start_state = current_state;
     for(var i=1;i<=res;i++){        
         //Intital starting original substring of result
         var out=Array.from(result).slice(0, Array.from(result).length-cps*i).join('')
@@ -66,6 +69,9 @@ async function startDelAnim(element){
 
         element.innerHTML=out
         await delay(totalTime/res)
+        if (start_state != current_state) {
+            return;
+        }
     }
     
     //After the animation
@@ -79,17 +85,27 @@ async function startDelAnim(element){
 
     Animates the sidebar to expand
 */
-async function expandSidebar(){
-    if (current_state == "expanded") {
+async function expandSidebar(){   
+    if (current_state == "manual_collapsing") {
         return;
     }
+
     document.getElementById("sidebar").classList.remove("collapseSidebar")
     document.getElementById("content").classList.remove("expandContent")
     document.getElementById("sidebar").classList.add("expandSidebar")
     document.getElementById("content").classList.add("collapseContent")
+
+    if (current_state == "expanded" || current_state == "expanding") {
+        return;
+    }
+    current_state = "expanding"
+    
     var animList=[['Profile','profileItem1'],['Projects','profileItem2']]
     await Promise.all(animList.map(element => startDelAnim(document.getElementById(element[1]))));
-
+    if (current_state != "expanding") {
+        return;
+    }
+    current_state = "empty_expanding"
     await Promise.all(animList.map(element => startMakeAnim(element[0], document.getElementById(element[1]))));
     current_state = "expanded";
 }
@@ -100,17 +116,26 @@ async function expandSidebar(){
 
     Animates the sidebar to collapse
 */
-async function collapseSidebar(){
-    if (current_state == "collapsed") {
-        return;
-    }
+async function collapseSidebar(manual=false){
     document.getElementById("sidebar").classList.remove("expandSidebar")
     document.getElementById("content").classList.remove("collapseContent")
     document.getElementById("sidebar").classList.add("collapseSidebar")
     document.getElementById("content").classList.add("expandContent")
-    var animList=[['👤','profileItem1'],['📄','profileItem2']]
-    await Promise.all(animList.map(element => startDelAnim(document.getElementById(element[1]))));
 
+    if (current_state == "collapsed" || current_state == "collapsing" || current_state == "manual_collapsing") {
+        return;
+    }
+    current_state = "collapsing"
+    if (manual == true ){
+        current_state = "manual_collapsing"
+    }
+    var animList=[['👤','profileItem1'],['📄','profileItem2']]
+
+    await Promise.all(animList.map(element => startDelAnim(document.getElementById(element[1]))));
+    if (current_state != "collapsing" && current_state != "manual_collapsing") {
+        return;
+    }
+    current_state = "empty_collapsing"
     await Promise.all(animList.map(element => startMakeAnim(element[0], document.getElementById(element[1]))));
     current_state = "collapsed";
 }
@@ -160,4 +185,5 @@ function set_sidebar_anim(){
     document.getElementById("sidebar").addEventListener("mouseover",expandSidebar);
     document.getElementById("sidebar").addEventListener("mouseleave",collapseSidebar);
     collapseSidebar();
+    
 }
