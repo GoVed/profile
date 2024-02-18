787f6ac6e3f865dd629596f4101e2e5704704786
Ved Suthar <vedhsuthar@gmail.com>
2024-02-18 01:13:56 -0500

    Fix relative paths

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ lazy_static! {
         valid_paths.insert("profileContent", "site/scripts/profileContent.js");
         valid_paths.insert("style", "site/styles/style.css");
         valid_paths.insert("projects", "site/layouts/projects.html");
+        valid_paths.insert("images", "site/images");
         valid_paths
     };
 }
@@ -29,14 +30,31 @@ async fn root() -> Result<NamedFile, NotFound<String>> {
     
 #[get("/<path..>")]
 async fn static_files(path: PathBuf) -> Result<NamedFile, NotFound<String>> {
-    if path.to_str().is_none() || !VALID_PATH.contains_key(path.to_str().unwrap()){
+    let mut rel_path = path.to_str().unwrap();
+    let root_path: &str;
+    match rel_path.split_once('/') {
+        Some((key, value)) => {
+            root_path = key;
+            rel_path = value;
+        }
+        None => {
+            root_path = rel_path;
+            rel_path = "";
+        }
+    }
+    println!("Root path: {}", root_path);
+    println!("Relative path: {}", rel_path);
+    if root_path == "" || !VALID_PATH.contains_key(root_path){
         NamedFile::open("site/layouts/index.html")
         .await
         .map_err(|e| NotFound(e.to_string()))
     }
     else{
-        let rpath = PathBuf::from(VALID_PATH.get(path.to_str().unwrap()).unwrap());
-        NamedFile::open(rpath).await.map_err(|e| NotFound(e.to_string()))    
+        let mut real_path = PathBuf::from(VALID_PATH.get(root_path).unwrap());
+        if rel_path != "" {
+            real_path.push(rel_path);
+        }
+        NamedFile::open(real_path).await.map_err(|e| NotFound(e.to_string()))    
     }
     
 }
